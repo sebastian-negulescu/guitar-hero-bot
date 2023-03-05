fe844803ad828537eb317af26433300fb5f3c573
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2023-03-04 20:35:00 -0500

    refactor main code

--- main.py
+++ main.py
@@ -3,19 +3,14 @@ import cv2 as cv
 import numpy as np
 
 def find_guitar( image ):
-    pass
-
-def main():
-    screen = cv.imread('./test.png')
-
-    dst = cv.Canny(screen, 50, 200, None, 3)
+    edges = cv.Canny( image, 50, 200, None, 3 )
 
     # Copy edges to the images that will display the results in BGR
-    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
-    cdstP = np.copy(cdst)
+    drawn_outline = cv.cvtColor( edges, cv.COLOR_GRAY2BGR )
 
-    lines = cv.HoughLines(dst, 1, np.pi / 180, 400, None, 0, 0)
+    lines = cv.HoughLines( edges, 1, np.pi / 180, 400, None, 0, 0 )
 
+    # draw the lines
     if lines is not None:
         for i in range(0, len(lines)):
             rho = lines[i][0][0]
@@ -26,10 +21,17 @@ def main():
             y0 = b * rho
             pt1 = (int(x0 + 5000*(-b)), int(y0 + 5000*(a)))
             pt2 = (int(x0 - 5000*(-b)), int(y0 - 5000*(a)))
-            cv.line(cdst, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+            cv.line(drawn_outline, pt1, pt2, (0,0,255), 3, cv.LINE_AA)
+
+    return drawn_outline 
+
+def main():
+    screen = cv.imread('./test.png')
+    cdst = find_guitar(screen) 
 
     cv.imshow('wimdow', cdst)
-
     cv.waitKey(0)
-
     cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
