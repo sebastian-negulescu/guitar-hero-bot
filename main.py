de8cfa658bf84e40a9e9c1006efa2f53543b3a50
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2023-04-07 12:00:21 -0400

    added crop image - not working

--- main.py
+++ main.py
@@ -39,10 +39,6 @@ def point_of_intersection(a, b):
 
 def find_guitar(image):
     edges = cv.Canny(image, 50, 200, None, 3)
-
-    # Copy edges to the images that will display the results in BGR
-    drawn_outline = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
-
     lines = cv.HoughLines(edges, 1, np.pi / 180, 400, None, 0, 0)
 
     filtered_lines = []
@@ -50,7 +46,9 @@ def find_guitar(image):
     if lines is not None:
         for line in lines:
             theta = line[0][1]
-            if not math.isclose(theta, math.pi / 2, rel_tol=EPSILON):
+            # np.pi / 2 is horizontal line, 0 is vertical
+            if not (math.isclose(theta, np.pi / 2, rel_tol=EPSILON) or 
+                    math.isclose(theta, 0, rel_tol=EPSILON)): # catch horizontal and vertical lines
                 filtered_lines.append(line)
 
     if len(filtered_lines) > 0: 
@@ -59,23 +57,54 @@ def find_guitar(image):
             for line_2 in filtered_lines:
                 theta_2 = line_2[0][1]
                 
-                angles_opposite = math.isclose(abs(math.pi - theta_1), theta_2, rel_tol=EPSILON)
+                angles_opposite = math.isclose(abs(np.pi - theta_1), theta_2, rel_tol=EPSILON)
 
                 if angles_opposite:
-                    poi_x = point_of_intersection(line_1, line_2)[0]
+                    poi = point_of_intersection(line_1, line_2)
+                    if poi is None:
+                        continue
                     cols = image.shape[1]
 
-                    if math.isclose(poi_x, cols, rel_tol=MIDDLE_TOLERANCE):
-                        return (line_1, line_2)
+                    if math.isclose(poi[0], cols, rel_tol=MIDDLE_TOLERANCE):
+                        # intersect guitar lines with base of image
+                        poi_1 = point_of_intersection(line_1, [[0, np.pi / 2]])
+                        poi_2 = point_of_intersection(line_2, [[0, np.pi / 2]])
+                        if poi_1 is None or poi_2 is None:
+                            return None
+                        return [poi, poi_1, poi_2]
+
+                    
+def crop_image(image, bounds):
+    # Create a mask with the same size as the image
+    mask = np.zeros_like(image)
+
+    # Create a white triangle on the mask
+    triangle = np.array(bounds, dtype=np.float32)
+    print(triangle)
+    cv.fillPoly(mask, [triangle], (255,255,255))
+
+    # Apply the mask to the image
+    result = cv.bitwise_and(image, mask)
+
+    # Display the cropped image
+    cv.imshow('Cropped Image', result)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 def find_notes(image, notes_path=DEFAULT_NOTES_PATH):
     # left to right
     # green, red, yellow, blue, orange
     # red note
-    note_colours = ['green', 'red', 'yellow', 'blue', 'orange']
-    for note_colour in note_colours:
-        note_filename = f'{note_colour}-note.png'
+    note_colours = [
+        ('green', (0, 255, 0)), 
+        ('red', (0, 0, 255)), 
+        ('yellow', (0, 255, 255)), 
+        ('blue', (255, 0, 0)), 
+        ('orange', (0, 165, 255))
+    ]
+    for note_colour_name, note_colour in note_colours:
+        note_filename = f'{note_colour_name}-note.png'
         note_path = os.path.join(notes_path, note_filename)
         note_template = cv.imread(note_path)
         h, w = note_template.shape[0:2]
@@ -85,21 +114,8 @@ def find_notes(image, notes_path=DEFAULT_NOTES_PATH):
         top_left = max_loc
         bottom_right = (top_left[0] + w, top_left[1] + h)
 
-        cv.rectangle(image, top_left, bottom_right, [255, 255, 255], 2)
+        cv.rectangle(image, top_left, bottom_right, note_colour, 2)
 
-    '''
-    red_note_name = 'red-note.png'
-    red_note_path = os.path.join(notes_path, red_note_name)
-    red_note_image = cv.imread(red_note_path)
-    h, w = red_note_image.shape[0:2]
-
-    red_result = cv.matchTemplate(image, red_note_image, cv.TM_CCOEFF)
-    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(red_result)
-    top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-
-    cv.rectangle(image, top_left, bottom_right, [255, 255, 255], 2)
-    '''
     cv.imshow('wimdow', image)
     cv.waitKey(0)
     cv.destroyAllWindows()
@@ -107,8 +123,9 @@ def find_notes(image, notes_path=DEFAULT_NOTES_PATH):
 
 def main():
     screen = cv.imread('./test.png')
-    guitar_lines = find_guitar(screen) 
-    find_notes(screen)
+    guitar_points = find_guitar(screen) 
+    crop_image(screen, guitar_points)
+    #find_notes(screen)
 
 if __name__ == "__main__":
     main()
