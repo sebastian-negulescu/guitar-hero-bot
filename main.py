5bba37e863aea962b1ecf570a9258a42da601891
Sebastian Negulescu <sebastian.a.negulescu@gmail.com>
2023-10-22 18:35:56 -0400

    implement queue messaging system

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import numpy as np
 
 from enum import Enum, auto
 
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 TICK_HZ = 30
+QUIT_VALUE = 113
 
 class NoteColours(Enum):
     GREEN = auto() 
@@ -17,24 +18,53 @@ class NoteColours(Enum):
     ORANGE = auto()
 
 
-def note_routine():
-    start_time = time.time()
+class TimeStamped:
+    def __init__(self, obj, curr_time = time.time()):
+        self.obj = obj
+        self.timestamp = curr_time
+
+
+def note_routine(frames):
     print('starting')
-    while time.time() < start_time + 30:
-        pass
+    while True: 
+        # check queue
+        try:
+            timestamped_obj = frames.get()
+            if timestamped_obj.obj == 'quit':
+                break
+        except ValueError:
+            # queue is closed
+            break
+
+        # check if the time is still valid
     print('finishing')
-    pass
 
 
 def shred():
     print('rock on!')
 
+    note_queues = {}
     note_detectors = {}
     for note_colour in NoteColours:
-        note_detectors[note_colour] = Process(target=note_routine)
+        note_queues[note_colour] = Queue()
+        note_detectors[note_colour] = Process(
+            target=note_routine, 
+            args=(note_queues[note_colour],)
+        )
 
     for note_colour in NoteColours:
         note_detectors[note_colour].start()
+
+    while True:
+        frame_time = time.time()
+        for note_colour in NoteColours:
+            pass
+
+        if cv.waitKey(1) == QUIT_VALUE:
+            break
+
+    for note_colour in NoteColours:
+        note_queues[note_colour].put(TimeStamped('quit'))
 
     for note_colour in NoteColours:
         note_detectors[note_colour].join()
